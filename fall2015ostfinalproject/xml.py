05d86e40e2d0dcba772d409d0c74872e82463a52
sh2514 <shu0018@live.com>
2015-12-16 00:47:29 -0500

    Added RSS link  implementation.

--- fall2015ostfinalproject/xml.py
+++ fall2015ostfinalproject/xml.py
@@ -18,41 +18,31 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
     
-class UserPageHandler(webapp2.RequestHandler):
+class XMLPageHandler(webapp2.RequestHandler):
   def post(self):
   	params = self.request.params
   	context = { }
   	
   	"""Generate log in/out information"""
   	context = login.generateLogInOutContextInfo(self, context)
-  	
-  	"""Get user information"""
-  	user = users.get_current_user();
-  	context['user'] = user;
-  	if user:
-  	  context['userId'] = user.user_id();
-  	  context['userEmail'] = user.email();
-  	  
-  	context['seekUserName'] = params['seekUserName'];
-  	context['seekUserId'] = params['seekUserId'];
-  	  
+
   	"""Query all sessions"""
   	sessions_query = sessions_datastore.Session.query(ancestor=sessions_datastore.sessions_key()).order(-sessions_datastore.Session.sessionStartTime);
   	sessions = sessions_query.fetch(1000000);
   	context['sessions'] = sessions;
   	
   	"""Query all reservations"""
-  	reservations_query = reservations_datastore.Reservation.query(ancestor=reservations_datastore.reservations_key()).order(-reservations_datastore.Reservation.reservationStartTime);
+  	reservations_query = reservations_datastore.Reservation.query(ancestor=reservations_datastore.reservations_key()).order(reservations_datastore.Reservation.reservationStartTime);
   	reservations = reservations_query.fetch(1000000);
   	context['reservations'] = reservations;
   	
-  	context['str'] = str;
+  	context['sessionGUID'] = params['sessionGUID'];
   	context['present'] = datetime.datetime.now();
-  	context['datetime'] = datetime; 
+  	context['datetime'] = datetime;  
   	
-  	contents = JINJA_ENVIRONMENT.get_template('user.html').render(context)
+  	contents = JINJA_ENVIRONMENT.get_template('xml.html').render(context)
   	self.response.write(contents)
   	
 app = webapp2.WSGIApplication([
-    ('/user.*', UserPageHandler)
+    ('/xml.*', XMLPageHandler)
 ], debug = True)
